8424de402c152d3ce5f352bc61bad22db03021e9
tel3port <carn3lian@gmail.com>
2024-04-02 16:44:52 +0300

    changed passport settings

--- utils/passport-setup.js
+++ utils/passport-setup.js
@@ -7,7 +7,7 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     // callbackURL: "http://127.0.0.1:3000/users/googlecloud-webhook",
-    callbackURL: "https://puzzlebook-creator-online.onrender.com/users/googlecloud-webhook"
+    callbackURL: "https://pzbkcrreator.onrender.com/users/googlecloud-webhook"
 
 }, authController.passportCallback))
 
