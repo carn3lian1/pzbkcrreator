b6bfbdb02bfd1ad90db610670666da646ae5a192
tel3port <carn3lian@gmail.com>
2024-04-03 11:50:42 +0300

    passport settings change

--- utils/passport-setup.js
+++ utils/passport-setup.js
@@ -6,8 +6,8 @@ const authController = require("../controllers/authController.js")
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://127.0.0.1:3000/users/googlecloud-webhook",
-    // callbackURL: "https://pzbkcrreator.onrender.com/users/googlecloud-webhook"
+    // callbackURL: "http://127.0.0.1:3000/users/googlecloud-webhook",
+    callbackURL: "https://pzbkcrreator.onrender.com/users/googlecloud-webhook"
 
 }, authController.passportCallback))
 
